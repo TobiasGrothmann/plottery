b025c917c21e95ff829fdfcff360b602840e9197
Tobias Grothmann <tobiasgrothmannplotterart@gmail.com>
2023-12-11 20:24:38 +0100

    sublayers

--- geom/src/layer/layer.rs
+++ geom/src/layer/layer.rs
@@ -1,28 +1,78 @@
-use std::slice::Iter;
-
 use itertools::Itertools;
+use std::slice::Iter;
 
 use crate::Shape;
 
 pub struct Layer {
     pub shapes: Vec<Box<dyn Shape>>,
+    pub sublayers: Vec<Layer>,
 }
 
 impl Layer {
     pub fn new() -> Self {
-        Self { shapes: Vec::new() }
+        Self {
+            shapes: Vec::new(),
+            sublayers: Vec::new(),
+        }
     }
 
     pub fn push<S: Shape + 'static>(&mut self, shape: S) {
         self.shapes.push(Box::new(shape));
     }
-
     pub fn push_boxed(&mut self, shape: Box<dyn Shape>) {
         self.shapes.push(shape);
     }
 
+    pub fn push_layer(&mut self, layer: Layer) {
+        self.sublayers.push(layer);
+    }
+
     pub fn iter(&self) -> Iter<'_, Box<dyn Shape>> {
         self.shapes.iter()
+    }
+
+    pub fn iter_sublayers(&self) -> Iter<Layer> {
+        self.sublayers.iter()
+    }
+    pub fn iter_flattened(&self) -> LayerFlattenedIterator {
+        LayerFlattenedIterator::new(&self)
+    }
+}
+
+pub struct LayerFlattenedIterator<'a> {
+    stack: Vec<&'a Layer>,
+    current_layer_iterator: Option<std::slice::Iter<'a, Box<dyn Shape>>>,
+}
+
+impl<'a> LayerFlattenedIterator<'a> {
+    fn new(layer: &'a Layer) -> LayerFlattenedIterator<'a> {
+        Self {
+            stack: vec![layer],
+            current_layer_iterator: None,
+        }
+    }
+}
+
+impl<'a> Iterator for LayerFlattenedIterator<'a> {
+    type Item = &'a dyn Shape;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(current_layer_iter) = &mut self.current_layer_iterator {
+                if let Some(shape) = current_layer_iter.next() {
+                    return Some(shape.as_ref());
+                }
+            }
+
+            if let Some(layer) = self.stack.pop() {
+                self.current_layer_iterator = Some(layer.shapes.iter());
+                for sublayer in layer.sublayers.iter() {
+                    self.stack.push(sublayer);
+                }
+            } else {
+                return None;
+            }
+        }
     }
 }
 
@@ -34,6 +84,7 @@ impl Clone for Layer {
                 .iter()
                 .map(|shape_box| shape_box.clone_box())
                 .collect_vec(),
+            sublayers: self.sublayers.clone(),
         }
     }
 }
@@ -51,6 +102,7 @@ impl FromIterator<Box<dyn Shape>> for Layer {
     fn from_iter<I: IntoIterator<Item = Box<dyn Shape>>>(iter: I) -> Self {
         Layer {
             shapes: iter.into_iter().collect(),
+            sublayers: Vec::new(),
         }
     }
 }
--- geom/src/layer/test.rs
+++ geom/src/layer/test.rs
@@ -1,5 +1,9 @@
 #[cfg(test)]
 mod test_layer {
+    use std::clone;
+
+    use itertools::Itertools;
+
     use crate::{layer::Layer, Circle, Path, Rect, SampleSettings, V2};
 
     #[test]
@@ -16,5 +20,46 @@ mod test_layer {
 
         let l2: Layer = l.clone();
         assert_eq!(l.iter().len(), l2.iter().len());
+    }
+
+    #[test]
+    fn children() {
+        let shape = Rect::new(V2::new(0.0, 0.0), V2::new(1.0, 1.0));
+
+        let mut l = Layer::new();
+        l.push(shape.clone());
+        assert_eq!(l.iter().len(), 1);
+
+        let mut l2 = Layer::new();
+        l2.push(shape.clone());
+        l2.push(shape.clone());
+        assert_eq!(l2.iter().len(), 2);
+
+        let mut l2_2 = Layer::new();
+        l2_2.push(shape.clone());
+        l2_2.push(shape.clone());
+        assert_eq!(l2_2.iter().len(), 2);
+        assert_eq!(l2_2.iter_sublayers().len(), 0);
+        assert_eq!(l2_2.iter_flattened().collect_vec().len(), 2);
+
+        let mut l3 = Layer::new();
+        l3.push(shape.clone());
+        l3.push(shape.clone());
+        l3.push(shape.clone());
+        assert_eq!(l3.iter().len(), 3);
+        l2_2.push_layer(l3);
+        assert_eq!(l2_2.iter().len(), 2);
+        assert_eq!(l2_2.iter_flattened().collect_vec().len(), 5);
+
+        l.push_layer(l2.clone());
+        l.push_layer(l2_2.clone());
+        assert_eq!(l.iter().len(), 1);
+        assert_eq!(l.iter_sublayers().len(), 2);
+        assert_eq!(l.iter_flattened().collect_vec().len(), 8);
+
+        let l_clone = l.clone();
+        assert_eq!(l_clone.iter().len(), 1);
+        assert_eq!(l_clone.iter_sublayers().len(), 2);
+        assert_eq!(l_clone.iter_flattened().collect_vec().len(), 8);
     }
 }
