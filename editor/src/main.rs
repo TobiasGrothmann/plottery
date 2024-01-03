ab2092a9dd250733a6b5ea52de8f403f1cad332a
Tobias Grothmann <tobiasgrothmannplotterart@gmail.com>
2024-01-03 16:38:19 +0100

    load app state

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,8 +1,33 @@
 #![allow(non_snake_case)]
+use std::path::PathBuf;
+
 use dioxus::prelude::*;
 use dioxus_desktop::{Config, LogicalSize, WindowBuilder, WindowCloseBehaviour};
+use plottery_cli::Project;
+
+use crate::app_state::AppState;
+use log::LevelFilter;
+
+mod app_state;
 
 fn main() {
+    dioxus_logger::DioxusLogger::new(LevelFilter::Info)
+        .use_format("[{LEVEL}] {PATH} - {ARGS}")
+        .build()
+        .expect("failed to init logger");
+
+    log::info!("Loading app state");
+    let mut app_state = AppState::load().unwrap_or_else(|| {
+        log::info!("App state file does not exist. Creating new app state.");
+        let new_state = AppState::new();
+        new_state.save();
+        new_state
+    });
+
+    app_state
+        .projects
+        .push(Project::new(PathBuf::from("."), "nonexisting".to_string()));
+
     dioxus_desktop::launch_cfg(
         App,
         Config::default()
