377e4571888efdd0d6a87a33cd69f87022eaa331
Tobias Grothmann <tobiasgrothmannplotterart@gmail.com>
2024-04-17 20:17:08 +0200

    update macro

--- project/project_macros/src/lib.rs
+++ project/project_macros/src/lib.rs
@@ -1,66 +1,132 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use syn;
+use syn::{self, Field, Ident};
 
-#[proc_macro_derive(PlotteryParamsDefinition)]
+#[proc_macro_derive(PlotteryParamsDefinition, attributes(default))]
 pub fn plottery_params(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     plottery_params_impl(&ast)
 }
 
-fn plottery_params_impl(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
-    let name = &ast.ident;
+fn perform_sanity_checks(data: &syn::DataStruct) {
+    for field in &data.fields {
+        match field.ty {
+            syn::Type::Array(_) => panic!("Parameter fields cannot be of type array."),
+            syn::Type::BareFn(_) => panic!("Parameter fields cannot be of type function."),
+            syn::Type::Group(_) => panic!("Parameter fields cannot be of type group."),
+            syn::Type::ImplTrait(_) => panic!("Parameter fields cannot be of type impl trait."),
+            syn::Type::Infer(_) => panic!("Parameter fields must have a specified type."),
+            syn::Type::Macro(_) => panic!("Parameter fields cannot be of type macro."),
+            syn::Type::Never(_) => panic!("Parameter fields cannot be of type never."),
+            syn::Type::Paren(_) => panic!("Parameter fields cannot be of type paren."),
+            syn::Type::Path(_) => {}
+            syn::Type::Ptr(_) => panic!("Parameter fields cannot be of type pointer."),
+            syn::Type::Reference(_) => panic!("Parameter fields cannot be of type reference."),
+            syn::Type::Slice(_) => panic!("Parameter fields cannot be of type slice."),
+            syn::Type::TraitObject(_) => panic!("Parameter fields cannot be of type trait object."),
+            syn::Type::Tuple(_) => panic!("Parameter fields cannot be of type tuple."),
+            syn::Type::Verbatim(_) => panic!("Parameter fields cannot be of type verbatim."),
+            _ => panic!("Parameter fields must have a specified type."),
+        }
+    }
+}
 
-    let data = match &ast.data {
-        syn::Data::Struct(data) => data,
-        syn::Data::Enum(_) => panic!("PlotteryProjectParams can only be derived for structs"),
-        syn::Data::Union(_) => panic!("PlotteryProjectParams can only be derived for structs"),
-    };
+fn get_field_type_name(field: &Field) -> String {
+    match &field.ty {
+        syn::Type::Path(path) => path
+            .path
+            .segments
+            .last()
+            .expect("Invalid field type.")
+            .ident
+            .to_string(),
+        _ => panic!("Parameter field type is invalid."),
+    }
+}
 
-    // GET PARAMS
+fn get_parameters_vector_items(data: &syn::DataStruct) -> Vec<proc_macro2::TokenStream> {
+    data.fields
+        .iter()
+        .map(|field| {
+            let field_type = &field.ty;
+            let default_value = quote!(#field_type::default());
+            let default_value = field
+                .attrs
+                .iter()
+                .find_map(|attr| {
+                    if attr.path().is_ident("default") {
+                        let value = &attr
+                            .meta
+                            .require_name_value()
+                            .expect("default attribute must have a value")
+                            .value;
+                        Some(quote!(#value))
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or(default_value);
 
-    let param_list = data
+            let field_name = field.ident.as_ref().unwrap().to_string();
+            quote! {
+                ProjectParam::new(#field_name, ProjectParamValue::Float(#default_value)),
+            }
+        })
+        .collect()
+}
+
+fn get_constructor_fields_items(data: &syn::DataStruct) -> Vec<proc_macro2::TokenStream> {
+    data
         .fields
         .iter()
         .map(|field| {
-            let field_name = field.ident.as_ref().unwrap().to_string();
+            let field_name = field.ident.as_ref().expect("Failed to access field.");
+            let field_type_name = get_field_type_name(field);
+            let accessor_function = Ident::new(&format!("get_{}", field_type_name), Span::call_site());
             quote! {
-                ProjectParam::new(#field_name, ProjectParamValue::Float(0.0)),
+                #field_name: params.get(stringify!(#field_name)).unwrap().value.#accessor_function().unwrap(),
             }
         })
-        .collect::<Vec<_>>();
+        .collect()
+}
 
+fn plottery_params_impl(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+    let name = &ast.ident;
+    let data = match &ast.data {
+        syn::Data::Struct(data) => data,
+        syn::Data::Enum(_) => {
+            panic!("PlotteryProjectParams cannot be derived for enums. Use a struct instead.")
+        }
+        syn::Data::Union(_) => {
+            panic!("PlotteryProjectParams cannot be derived for unions. Use a struct instead.")
+        }
+    };
+
+    // SANITY CHECKS
+    perform_sanity_checks(data);
+
+    // GET PARAMS
+    let parameter_vector_items = get_parameters_vector_items(data);
     let get_params_impl = quote! {
         fn get_params(&self) -> Vec<ProjectParam> {
             vec![
-                #(#param_list)*
+                #(#parameter_vector_items)*
             ]
         }
     };
 
     // NEW FROM LIST
-
-    let constructor_list = data
-        .fields
-        .iter()
-        .map(|field| {
-            let field_name = &field.ident;
-            quote! {
-                #field_name: params.get(stringify!(#field_name)).unwrap().value.get_float().unwrap(),
-            }
-        })
-        .collect::<Vec<_>>();
-
+    let constructor_fields = get_constructor_fields_items(data);
     let new_from_list_impl = quote! {
         fn new_from_list(params: &std::collections::HashMap<String, ProjectParam>) -> Self {
             Self {
-                #(#constructor_list)*
+                #(#constructor_fields)*
             }
         }
     };
 
     // TRAIT IMPLEMENTATION
-
     let expanded = quote! {
         impl PlotteryParamsDefinition for #name {
             #get_params_impl
--- project/project_params/src/project_param_value.rs
+++ project/project_params/src/project_param_value.rs
@@ -5,69 +5,23 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ProjectParamValue {
     Float(f32),
-    // RangedFloat { val: f32, min: f32, max: f32 },
     Int(i32),
-    // RangedInt { val: i32, min: i32, max: i32 },
     V2(V2),
-    // String(String),
-    // Bool(bool),
-    // Enum { val: String, options: Vec<String> },
 }
 
 impl ProjectParamValue {
-    pub fn get_float(&self) -> Result<f32> {
+    pub fn get_f32(&self) -> Result<f32> {
         match self {
             ProjectParamValue::Float(val) => Ok(*val),
             _ => Err(anyhow::anyhow!("Value is not a float")),
         }
     }
 
-    // pub fn get_ranged_float(&self) -> Result<f32> {
-    //     match self {
-    //         ProjectParamValue::RangedFloat { val, min, max } => Ok(val.clamp(*min, *max)),
-    //         _ => Err(anyhow::anyhow!("Value is not a ranged float")),
-    //     }
-    // }
-
-    pub fn get_int(&self) -> Result<i32> {
-        match self {
-            ProjectParamValue::Int(val) => Ok(*val),
-            _ => Err(anyhow::anyhow!("Value is not an int")),
-        }
-    }
-
-    // pub fn get_ranged_int(&self) -> Result<i32> {
-    //     match self {
-    //         ProjectParamValue::RangedInt { val, min, max } => Ok((*val).clamp(*min, *max)),
-    //         _ => Err(anyhow::anyhow!("Value is not a ranged int")),
-    //     }
-    // }
-
-    pub fn get_v2(&self) -> Result<V2> {
+    #[allow(non_snake_case)]
+    pub fn get_V2(&self) -> Result<V2> {
         match self {
             ProjectParamValue::V2(val) => Ok(*val),
             _ => Err(anyhow::anyhow!("Value is not a V2")),
         }
     }
-
-    // pub fn get_string(&self) -> Result<String> {
-    //     match self {
-    //         ProjectParamValue::String(val) => Ok(val.clone()),
-    //         _ => Err(anyhow::anyhow!("Value is not a string")),
-    //     }
-    // }
-
-    // pub fn get_bool(&self) -> Result<bool> {
-    //     match self {
-    //         ProjectParamValue::Bool(val) => Ok(*val),
-    //         _ => Err(anyhow::anyhow!("Value is not a bool")),
-    //     }
-    // }
-
-    // pub fn get_enum(&self) -> Result<String> {
-    //     match self {
-    //         ProjectParamValue::Enum { val, .. } => Ok(val.clone()),
-    //         _ => Err(anyhow::anyhow!("Value is not an enum")),
-    //     }
-    // }
 }
