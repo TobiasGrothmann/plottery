faae0331ccf624355ac5c2b4f5bc5d96dd5c16c6
Tobias Grothmann <tobiasgrothmannplotterart@gmail.com>
2024-04-22 17:48:16 +0200

    WIP slider for ranged values

--- editor/src/router_components/editor.rs
+++ editor/src/router_components/editor.rs
@@ -246,36 +246,90 @@ pub fn Editor(project_path: String) -> Element {
                     for param in params.read().list.iter().cloned() {
                         div { class: "param",
                             p { "{param.name.clone()}" }
-                            input {
-                                name: "{param.name.clone()}",
-                                required: true,
-                                value: match param.value {
-                                    ProjectParamValue::Float(val) => val.to_string(),
-                                    ProjectParamValue::FloatRanged { val, min: _, max: _ } => val.to_string(),
-                                    ProjectParamValue::Int(val) => val.to_string(),
-                                    ProjectParamValue::IntRanged { val, min: _, max: _ } => val.to_string(),
-                                },
-                                placeholder: "value",
-                                onchange: move |event| {
-                                    let mut new_params = params.read().clone();
-                                    for param_field in new_params.list.iter_mut() {
-                                        if param_field.name == param.name.clone() {
-                                            let new_val = event.value().parse().unwrap();
-                                            match param_field.value {
-                                                ProjectParamValue::Float(_) => param_field.value.set_f32(new_val),
-                                                ProjectParamValue::FloatRanged { .. } => param_field.value.set_f32(new_val),
-                                                ProjectParamValue::Int(_) => param_field.value.set_i32(new_val.round() as i32),
-                                                ProjectParamValue::IntRanged { .. } => param_field.value.set_i32(new_val.round() as i32),
+
+                            match param.value {
+                                ProjectParamValue::Float(_) | ProjectParamValue::Int(_) => {
+                                    // value
+                                    rsx! {
+                                        input {
+                                            name: "{param.name.clone()}",
+                                            placeholder: "value",
+                                            required: true,
+                                            value: match param.value {
+                                                ProjectParamValue::Float(val) => val.to_string(),
+                                                ProjectParamValue::Int(val) => val.to_string(),
+                                                _ => panic!("Unexpected Error"),
+                                            },
+                                            onchange: move |event| {
+                                                let mut new_params = params.read().clone();
+                                                for param_field in new_params.list.iter_mut() {
+                                                    if param_field.name == param.name.clone() {
+                                                        let new_val = event.value().parse().unwrap();
+                                                        match param_field.value {
+                                                            ProjectParamValue::Float(_) => param_field.value.set_f32(new_val),
+                                                            ProjectParamValue::Int(_) => param_field.value.set_i32(new_val.round() as i32),
+                                                            _ => panic!("Unexpected Error"),
+                                                        }
+                                                    }
+                                                }
+                                                params.set(new_params);
+                                                match project_runner.read().try_lock() {
+                                                    Ok(mut runner) => runner.trigger_run_project(release, running_state),
+                                                    Err(e) => {
+                                                        log::error!("Error preparing to run: {:?}", e);
+                                                        running_state.set(RunningState::RunFailed { msg: format!("Error preparing to run: {}", e) });
+                                                    },
+                                                }
                                             }
                                         }
                                     }
-                                    params.set(new_params);
-                                    match project_runner.read().try_lock() {
-                                        Ok(mut runner) => runner.trigger_run_project(release, running_state),
-                                        Err(e) => {
-                                            log::error!("Error preparing to run: {:?}", e);
-                                            running_state.set(RunningState::RunFailed { msg: format!("Error preparing to run: {}", e) });
-                                        },
+                                }
+                                ProjectParamValue::FloatRanged { .. } | ProjectParamValue::IntRanged { .. } => {
+                                    // slider
+                                    rsx! {
+                                        input {
+                                            class: "slider",
+                                            name: "{param.name.clone()}",
+                                            required: true,
+                                            r#type: "range",
+                                            step: "0.01",
+                                            min: match param.value {
+                                                ProjectParamValue::FloatRanged { val: _, min, max: _ } => min.to_string(),
+                                                ProjectParamValue::IntRanged { val: _, min, max: _ } => min.to_string(),
+                                                _ => panic!("Unexpected Error"),
+                                            },
+                                            max: match param.value {
+                                                ProjectParamValue::FloatRanged { val: _, min: _, max } => max.to_string(),
+                                                ProjectParamValue::IntRanged { val: _, min: _, max } => max.to_string(),
+                                                _ => panic!("Unexpected Error"),
+                                            },
+                                            value: match param.value {
+                                                ProjectParamValue::FloatRanged { val, min: _, max: _ } => val.to_string(),
+                                                ProjectParamValue::IntRanged { val, min: _, max: _ } => val.to_string(),
+                                                _ => panic!("Unexpected Error"),
+                                            },
+                                            onchange: move |event| {
+                                                let mut new_params = params.read().clone();
+                                                for param_field in new_params.list.iter_mut() {
+                                                    if param_field.name == param.name.clone() {
+                                                        let new_val = event.value().parse().unwrap();
+                                                        match param_field.value {
+                                                            ProjectParamValue::FloatRanged { .. } => param_field.value.set_f32(new_val),
+                                                            ProjectParamValue::IntRanged { .. } => param_field.value.set_i32(new_val.round() as i32),
+                                                            _ => panic!("Unexpected Error"),
+                                                        }
+                                                    }
+                                                }
+                                                params.set(new_params);
+                                                match project_runner.read().try_lock() {
+                                                    Ok(mut runner) => runner.trigger_run_project(release, running_state),
+                                                    Err(e) => {
+                                                        log::error!("Error preparing to run: {:?}", e);
+                                                        running_state.set(RunningState::RunFailed { msg: format!("Error preparing to run: {}", e) });
+                                                    },
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
