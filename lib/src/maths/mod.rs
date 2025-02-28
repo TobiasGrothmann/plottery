b2bf2f153794050bad25d79e6e90df193ef3eb9c
Tobias Grothmann <tobias.grothmann@googlemail.com>
2025-02-28 14:48:32 +0100

    float util

--- lib/src/maths/mod.rs
+++ lib/src/maths/mod.rs
@@ -1,10 +1,13 @@
 pub mod consts;
+pub mod float;
+mod float_test;
 pub mod noise;
 mod noise_test;
 pub mod random;
 mod thread_local;
 
 pub use consts::*;
+pub use float::*;
 pub use noise::*;
 pub use random::*;
 pub use thread_local::{seed, seed_random};
