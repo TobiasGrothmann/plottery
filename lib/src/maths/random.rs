491416c514556de0c3f1ab33a1a2375c8f1653cf
Tobias Grothmann <tobias.grothmann@googlemail.com>
2025-03-07 10:22:02 +0100

    docs rand

--- lib/src/maths/random.rs
+++ lib/src/maths/random.rs
@@ -3,18 +3,22 @@ use rand_distr::{Distribution, Normal, SkewNormal};
 
 use super::thread_local::RNG;
 
+/// uniform random float between `from` and `to`
 pub fn rand_range(from: f32, to: f32) -> f32 {
     RNG.with_borrow_mut(|rng: &mut rand::prelude::StdRng| rng.gen_range(from..to))
 }
 
+/// uniform random integer between `from` and `to`
 pub fn rand_range_i(from: i32, to: i32) -> i32 {
     RNG.with_borrow_mut(|rng: &mut rand::prelude::StdRng| rng.gen_range(from..to))
 }
 
+/// random boolean with given `chance` of being true
 pub fn coin(chance: f32) -> bool {
     RNG.with_borrow_mut(|rng| rng.gen::<f32>() < chance)
 }
 
+/// see [`rand_distr::Normal`]
 pub fn rand_normal(mean: f32, std_dev: f32) -> f32 {
     let normal = Normal::new(mean, std_dev).unwrap_or_else(|_| {
         panic!(
@@ -25,6 +29,7 @@ pub fn rand_normal(mean: f32, std_dev: f32) -> f32 {
     RNG.with_borrow_mut(|rng| normal.sample(rng))
 }
 
+/// see [`rand_distr::SkewNormal`]
 pub fn rand_normal_skewed(location: f32, scale: f32, shape: f32) -> f32 {
     let normal_skewed = SkewNormal::new(location, scale, shape).unwrap_or_else(|_| {
         panic!(
@@ -35,6 +40,7 @@ pub fn rand_normal_skewed(location: f32, scale: f32, shape: f32) -> f32 {
     RNG.with_borrow_mut(|rng| normal_skewed.sample(rng))
 }
 
+/// see [`rand_distr::Exp`]
 pub fn rand_exponential(lambda: f32) -> f32 {
     let exponential = rand_distr::Exp::new(lambda).unwrap_or_else(|_| {
         panic!(
