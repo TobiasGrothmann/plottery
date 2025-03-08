db8e705ea7ce1cbcacefd9ed9493e5230df86850
Tobias Grothmann <tobias.grothmann@googlemail.com>
2025-03-08 10:09:51 +0100

    fix exports for tests

--- project/src/project_params/mod.rs
+++ project/src/project_params/mod.rs
@@ -3,3 +3,8 @@ pub mod project_param_value;
 pub mod project_params_definition;
 pub mod project_params_list_wrapper;
 mod project_params_list_wrapper_test;
+
+pub use project_param::*;
+pub use project_param_value::*;
+pub use project_params_definition::*;
+pub use project_params_list_wrapper::*;
