317c3586a6a5d0bf87544195281764c0fa1cfa4f
Tobias Grothmann <tobias.grothmann@googlemail.com>
2025-03-22 10:04:51 +0100

    update project template

--- project/cargo_project_template/src/generate.rs
+++ project/cargo_project_template/src/generate.rs
@@ -1,8 +1,7 @@
 use plottery_lib::*;
 use plottery_project::*;
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PlotteryParams, Serialize, Deserialize, PartialEq)]
+#[derive(PlotteryParams)]
 pub struct Params {
     #[value(0.1)]
     #[range(0.02, 0.35)]
@@ -15,10 +14,11 @@ pub struct Params {
 }
 
 pub fn generate(params: Params) -> Layer {
-    // setup
     let mut l = Layer::new();
     let size = V2::din_a(params.din_size as u8);
     let frame = Frame::new_xy(size, size.min_axis() * 0.12);
+
+    let spiral_center = frame.center();
 
     // create circles in a spiral
     let mut circles = vec![];
@@ -26,12 +26,12 @@ pub fn generate(params: Params) -> Layer {
     let mut i = 0;
     let mut distance = 0.0;
     let mut angle = Angle::zero();
-    while distance < size.max_axis() * 1.5 {
+    while distance < frame.inner_rect().max_dist_to_any_corner(spiral_center) {
         i += 1;
         distance = (i as f32).sqrt() * 0.5;
         angle = (angle + Angle::golden_ratio()).mod_one_rotation();
         let pos = V2::polar(angle, distance);
-        circles.push(Circle::new(size * 0.5 + pos, params.circle_size));
+        circles.push(Circle::new(spiral_center + pos, params.circle_size));
     }
 
     // generate plot with only the circles that fit in the frame
